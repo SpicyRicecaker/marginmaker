1ee58c54967a381e80415994e277fddb23637f49
SpicyRicecaker <awesomenessisgolden@gmail.com>
2026-07-10 23:29:10 -0700

    Cleanup comments from annot testing

--- main2.py
+++ main2.py
@@ -6,8 +6,6 @@ import sys
 from pathlib import Path
 from pypdf import PdfReader, PdfWriter, Transformation
 from pypdf.generic import FloatObject, NameObject, ArrayObject
-from pprint import pprint
-
 
 MARGIN_SIDE_PT = 500
 
@@ -21,14 +19,7 @@ def copy_outline(reader, writer, outline, parent=None):
             page_num = reader.get_destination_page_number(item)
             last = writer.add_outline_item(item.title, page_num, parent=parent)
 
-# inklists = []
-# should_break = False
-
 def return_transformed(A, tx, ty):
-    # print('---------------------------------------')
-    # pprint(A)
-    # print('---------------------------------------')
-
     i = 0
     A_prime = []
 
@@ -54,9 +45,6 @@ def convert(input_pdf, output_pdf):
     writer = PdfWriter()
 
     for page in reader.pages:
-        # global should_break
-        # if should_break:
-        #     break
         # get the page width and height
         w, h = float(page.mediabox.width) + MARGIN_SIDE_PT, float(page.mediabox.height)
         # calculate how much we need to transform the page
@@ -67,68 +55,21 @@ def convert(input_pdf, output_pdf):
         new_page.merge_transformed_page(
             page, Transformation().translate(tx, ty)
         )
-        # --- FIX: Use NameObject for keys and ArrayObject for lists ---
         if "/Annots" in new_page:
             for annot_ref in new_page["/Annots"]:
                 annot = annot_ref.get_object()
                 
-                # Shift /Rect using NameObject and ArrayObject
-                # if "/Rect" in annot:
-                #     rect = annot["/Rect"]
-                #     rect_key = NameObject("/Rect")
-                #     rect_values = ArrayObject([
-                #         FloatObject(float(rect[0]) + tx),
-                #         FloatObject(float(rect[1]) + ty),
-                #         FloatObject(float(rect[2]) + tx),
-                #         FloatObject(float(rect[3]) + ty)
-                #     ])
-                #     annot[rect_key] = rect_values
-
                 if "/InkList" in annot:
-                    # global inklists
-                    # inklists.append(annot["/InkList"])
-                    # pprint(annot["/InkList"])
-                    # print("-----------------")
-                    # for a in annot["/InkList"]:
-                    #     print(a)
                     inklist = annot["/InkList"]
                     inklist_key = NameObject("/InkList")
                     new_inklist = return_transformed(inklist, tx, ty)
-                    # print(new_inklist)
                     annot[inklist_key] = new_inklist
-                    # pprint(return_transformed(annot["/InkList"], tx, ty))
-                    # exit()
-                    # pprint(len(inklists))
-                    # if len(inklists) > 5:
-                    #     should_break = True
-                    #     break
 
                 if "/Path" in annot:
                     path = annot["/Path"]
                     path_key = NameObject("/Path")
                     new_path = return_transformed(path, tx, ty)
-                    # print(new_inklist)
                     annot[path_key] = new_path
-
-                # Shift /QuadPoints using NameObject and ArrayObject
-                # global test
-                # for k in dict.keys(annot):
-                #     test.add(k)
-                # if "/QuadPoints" in annot:
-                #     qp = annot["/QuadPoints"]
-                #     qp_key = NameObject("/QuadPoints")
-                #     new_qp = []
-                #     for i, coord in enumerate(qp):
-                #         if i % 2 == 0:
-                #             new_qp.append(FloatObject(float(coord) + tx))
-                #         else:
-                #             new_qp.append(FloatObject(float(coord) + ty))
-                #     annot[qp_key] = ArrayObject(new_qp)
-                # if "/AP" in annot:
-                #     del annot["/AP"]
-    # pprint(inklists)
-    # exit()
-    # -----------------------------------------------------------
     if reader.outline:
         # add metadatga
         copy_outline(reader, writer, reader.outline)
